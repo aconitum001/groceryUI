86be91741cc36763e6c6ae0629a582e6094a38ef
aconitum001 <skandern0000@gmail.com>
2024-11-10 17:50:43 +0100

    generated app assets

---
+++ lib/utils/assets.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesApple
+  /// assets/images/apple.png
+  static const String assetsImagesApple = "assets/images/apple.png";
+
+  /// Assets for assetsImagesBroccoli
+  /// assets/images/broccoli.png
+  static const String assetsImagesBroccoli = "assets/images/broccoli.png";
+
+  /// Assets for assetsImagesBrocoli
+  /// assets/images/brocoli.png
+  static const String assetsImagesBrocoli = "assets/images/brocoli.png";
+
+  /// Assets for assetsImagesButternut
+  /// assets/images/butternut.png
+  static const String assetsImagesButternut = "assets/images/butternut.png";
+
+  /// Assets for assetsImagesCarrots
+  /// assets/images/carrots.png
+  static const String assetsImagesCarrots = "assets/images/carrots.png";
+
+  /// Assets for assetsImagesCheese
+  /// assets/images/cheese.png
+  static const String assetsImagesCheese = "assets/images/cheese.png";
+
+  /// Assets for assetsImagesGinger
+  /// assets/images/ginger.png
+  static const String assetsImagesGinger = "assets/images/ginger.png";
+
+  /// Assets for assetsImagesIntroBackground
+  /// assets/images/Intro_background.png
+  static const String assetsImagesIntroBackground = "assets/images/Intro_background.png";
+
+  /// Assets for assetsImagesLambMeat
+  /// assets/images/lamb_meat.png
+  static const String assetsImagesLambMeat = "assets/images/lamb_meat.png";
+
+  /// Assets for assetsImagesLettuce
+  /// assets/images/lettuce.png
+  static const String assetsImagesLettuce = "assets/images/lettuce.png";
+
+  /// Assets for assetsImagesMeat
+  /// assets/images/meat.png
+  static const String assetsImagesMeat = "assets/images/meat.png";
+
+  /// Assets for assetsImagesOfferContainerBackground
+  /// assets/images/offer_container_background.png
+  static const String assetsImagesOfferContainerBackground = "assets/images/offer_container_background.png";
+
+  /// Assets for assetsImagesPeper
+  /// assets/images/peper.png
+  static const String assetsImagesPeper = "assets/images/peper.png";
+}
+
